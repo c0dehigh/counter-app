168470b910da0d365e7f21ff3f36a2e6c7617c2a
Hamed <high_dev@outlook.com>
2022-05-14 19:58:35 +0430

    Added counter jsx

--- src/Toplearn.js
+++ src/Toplearn.js
@@ -1,8 +1,11 @@
+import Counter from "./components/Counter";
+
 import './Toplearn.css'
 const Toplearn = () => {
     return (
         <div className="Toplearn">
             <h1>شمارنده من</h1>
+            <Counter/>
         </div>
     )
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+const Counter = () => {
+    return (
+        <div>
+            <p>
+                {Math.floor(Math.random() * 1000)}  شمارنده :
+            </p>
+        </div>
+    )
+}
+
+
+export default Counter
