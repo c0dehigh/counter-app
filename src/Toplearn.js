be657fd39a049cb16baacf09e7cd8f4343003094
Hamed <high_dev@outlook.com>
2022-05-14 13:00:01 +0430

    removed and added new files

---
+++ src/Toplearn.js
@@ -0,0 +1,10 @@
+import './Toplearn.css'
+const Toplearn = () => {
+    return (
+        <div className="Toplearn">
+            <h1>شمارنده من</h1>
+        </div>
+    )
+}
+
+export default Toplearn
