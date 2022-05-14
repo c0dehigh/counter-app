0c066beffbec3b63db018f970a7c6e5b404e7470
Hamed <high_dev@outlook.com>
2022-05-14 20:29:15 +0430

    try component class base

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,26 @@
-const Counter = () => {
-    return (
-        <div>
-            <p>
-                شمارنده :    {Math.floor(Math.random() * 1000)}
+import {Component} from "react";
+
+
+// const Counter = () => {
+//     return (
+//         <div>
+//             <p>
+//                 شمارنده :    {Math.floor(Math.random() * 1000)}
+//             </p>
+//         </div>
+//     )
+// }
+
+class Counter extends Component {
+    render() {
+        return(
+                     <div>
+             <p>
+                 شمارنده :    {Math.floor(Math.random() * 1000)}
             </p>
-        </div>
-    )
+         </div>
+        )
+    }
 }
 
 
