6c8ac8a5d5806c16953ede11f966210c0e955526
Hamed <high_dev@outlook.com>
2022-05-15 15:16:59 +0430

    Added functional state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import { Component } from "react";
+import { useState } from "react";
 
-class App extends Component {
-  state = {
-    count: 0,
-    name: "Hamed",
-    family: "Zibandeh",
-  };
-  render() {
-    const { count, name, family } = this.state;
-    return (
-      <div>
-        <header>
-          <h1>شمارنده من</h1>
-        </header>
-        <p>{count}</p>
-        <p>{name}</p>
-        <p>{family}</p>
-      </div>
-    );
-  }
-}
+
+
+const App = () => {
+  const [count,setCount] = useState(5)
+
+  
+  return (
+    <div>
+      <header>
+        <h1>شمارنده من</h1>
+      </header>
+      <p>{count}</p>
+      
+    </div>
+  );
+};
+
+// class App extends Component {
+//   state = {
+//     count: 20,
+//     name: "Alex",
+//     family: "Morfi",
+//   };
+//   render() {
+//     const { count, name, family } = this.state;
+//     return (
+//       <div>
+//         <header>
+//           <h1>شمارنده من</h1>
+//         </header>
+//         <p>{count}</p>
+//         <p>{name}</p>
+//         <p>{family}</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
