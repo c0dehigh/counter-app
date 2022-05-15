cb54ef68cba5a50d7b47211858c3c0d3e501764f
Hamed <high_dev@outlook.com>
2022-05-15 15:33:11 +0430

    added counter componet

---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <p>Counter</p>
+      </div>
+    );
+  }
+}
+
+export default Counter;
