e5f8b64d5674db68a250d409759d4710932e2a5c
Hamed <high_dev@outlook.com>
2022-05-17 17:43:25 +0430

    added classNames Module

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,21 @@
 import propTypes from 'prop-types'
+import classNames from "classnames";
+
+import './Counter.css'
 
 const Counter = ({inc,dec,reset,count}) => {
+// added classNames external module
+    let testStyle = classNames({
+        'red' : count === 0,
+        'teal' : count !==0
+    })
 
   return(
 
       <div>
         <h1 style={{color: count ===0? 'red' : 'green'}}>{count}</h1>
         <button onClick={inc}>+</button>
-        <button style={{backgroundColor: count === 0 ? 'red' : 'teal'}} onClick={dec} disabled={count === 0}>-</button>
+        <button className={testStyle} onClick={dec} disabled={count === 0}>-</button>
         <br/>
         <button onClick={reset}>Reset</button>
       </div>
